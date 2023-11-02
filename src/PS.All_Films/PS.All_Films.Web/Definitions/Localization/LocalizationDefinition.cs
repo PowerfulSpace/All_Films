22858b4fbd312f3345aab60739ec2b1173d4f84f
Alexander <sany199574@gmail.com>
2023-11-03 00:25:45 +0300

    Добавлена локализация en-Us и es-ES  Локализация en-US нужна для правильнйо работы с разделяющим знаком число
    Исправлена ошибка с представлением
    - Create

---
+++ src/PS.All_Films/PS.All_Films.Web/Definitions/Localization/LocalizationDefinition.cs
@@ -0,0 +1,33 @@
+﻿using PS.All_Films.Web.Definitions.Base;
+using System.Globalization;
+
+namespace PS.All_Films.Web.Definitions.Localization
+{
+    public class LocalizationDefinition : AppDefinitions
+    {
+        private RequestLocalizationOptions localizationOptions = null!;
+
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddLocalization();
+
+            localizationOptions = new RequestLocalizationOptions();
+
+            var supportedCultures = new[]
+           {
+                new CultureInfo("en-US"),
+                new CultureInfo("es-ES")
+            };
+
+            localizationOptions.SupportedCultures = supportedCultures;
+            localizationOptions.SupportedUICultures = supportedCultures;
+            localizationOptions.SetDefaultCulture("en-US");
+            localizationOptions.ApplyCurrentCultureToResponseHeaders = true;
+        }
+
+        public override void ConfigureApplication(WebApplication app, IWebHostEnvironment environment)
+        {
+            app.UseRequestLocalization(localizationOptions);
+        }
+    }
+}
